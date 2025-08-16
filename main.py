36f1bac3e132959e602c7371760a9685ac2dbb43
Luis Guilherme Pelinson Giani <lggiani@gmail.com>
2025-08-16 16:42:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ def soma():
 def subtracao():
     resultado = num1 - num2
 
+def multiplicacao():
+    resultado = num1 * num2
 
+def divisao():
+    resultado = num1 / num2
 
 def main():
     print('teste')
