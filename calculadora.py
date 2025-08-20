a1fbfaa5120165b2b131541c0a20b7418c2be40e
Luis Guilherme Pelinson Giani <lggiani@gmail.com>
2025-08-19 21:45:43 -0300

    Reestruturação das funções

--- calculadora.py
+++ calculadora.py
@@ -2,29 +2,28 @@ import flet as ft
 from flet import Colors
 
 botoes = [
-{'operador': 'AC', 'fonte': 'black', 'fundo': 'white'},
-{'operador': '±', 'fonte': 'black', 'fundo': 'white'},
-{'operador': '%', 'fonte': 'black', 'fundo': 'white'},
-{'operador': '/', 'fonte': 'white', 'fundo': 'orange'},
-{'operador': '7', 'fonte': 'white', 'fundo': ft.Colors.WHITE24},
-{'operador': '8', 'fonte': 'white', 'fundo': ft.Colors.WHITE24},
-{'operador': '9', 'fonte': 'white', 'fundo': ft.Colors.WHITE24},
-{'operador': '*', 'fonte': 'white', 'fundo': 'orange'},
-{'operador': '4', 'fonte': 'white', 'fundo': ft.Colors.WHITE24},
-{'operador': '5', 'fonte': 'white', 'fundo': ft.Colors.WHITE24},
-{'operador': '6', 'fonte': 'white', 'fundo': ft.Colors.WHITE24},
-{'operador': '-', 'fonte': 'white', 'fundo': 'orange'},
-{'operador': '1', 'fonte': 'white', 'fundo': ft.Colors.WHITE24},
-{'operador': '2', 'fonte': 'white', 'fundo': ft.Colors.WHITE24},
-{'operador': '3', 'fonte': 'white', 'fundo': ft.Colors.WHITE24},
-{'operador': '+', 'fonte': 'white', 'fundo': 'orange'},
-{'operador': '0', 'fonte': 'white', 'fundo': ft.Colors.WHITE24},
-{'operador': '.', 'fonte': 'white', 'fundo': ft.Colors.WHITE24},
-{'operador': '=', 'fonte': 'white', 'fundo': 'orange'}
+    {'operador': 'AC', 'fonte': 'black', 'fundo': 'white'},
+    {'operador': '±', 'fonte': 'black', 'fundo': 'white'},
+    {'operador': '%', 'fonte': 'black', 'fundo': 'white'},
+    {'operador': '/', 'fonte': 'white', 'fundo': 'orange'},
+    {'operador': '7', 'fonte': 'white', 'fundo': ft.Colors.WHITE24},
+    {'operador': '8', 'fonte': 'white', 'fundo': ft.Colors.WHITE24},
+    {'operador': '9', 'fonte': 'white', 'fundo': ft.Colors.WHITE24},
+    {'operador': '*', 'fonte': 'white', 'fundo': 'orange'},
+    {'operador': '4', 'fonte': 'white', 'fundo': ft.Colors.WHITE24},
+    {'operador': '5', 'fonte': 'white', 'fundo': ft.Colors.WHITE24},
+    {'operador': '6', 'fonte': 'white', 'fundo': ft.Colors.WHITE24},
+    {'operador': '-', 'fonte': 'white', 'fundo': 'orange'},
+    {'operador': '1', 'fonte': 'white', 'fundo': ft.Colors.WHITE24},
+    {'operador': '2', 'fonte': 'white', 'fundo': ft.Colors.WHITE24},
+    {'operador': '3', 'fonte': 'white', 'fundo': ft.Colors.WHITE24},
+    {'operador': '+', 'fonte': 'white', 'fundo': 'orange'},
+    {'operador': '0', 'fonte': 'white', 'fundo': ft.Colors.WHITE24},
+    {'operador': '.', 'fonte': 'white', 'fundo': ft.Colors.WHITE24},
+    {'operador': '=', 'fonte': 'white', 'fundo': 'orange'}
 ]
 
-
-def main (page: ft.Page):
+def main(page: ft.Page):
     page.bgcolor = 'black'
     page.window.resizable = False
     page.window.width = 430
@@ -35,35 +34,44 @@ def main (page: ft.Page):
     resultado = ft.Text('0', size=40, color='white')
 
     def calculate(operador, valor_atual):
-        value = eval(valor_atual)
-
-        if operador == '%':
-            value /= 100
-        elif operador == '±':
-            value = -value
-        
-        return value
+        try:
+            value = eval(valor_atual)
+            
+            if operador == '%':
+                value /= 100
+            elif operador == '±':
+                value = -value
+            
+            return str(value)
+        except:
+            return 'Erro'
 
     def select(e):
-        valor_atual = resultado.value if resultado.value != '0' else ''
+        valor_atual = resultado.value
         value = e.control.content.value
-
-        if value.isdigit():
-            value = valor_atual + value
-
-        elif value == 'AC':
-            value = '0'
-        else:
-            if valor_atual and valor_atual[-1] in ('/','*','-','+','.'):
-                valor_atual = valor_atual[:-1]
-
-            value = valor_atual + value
-
-            if value[-1] in ('=', '%', '±'):
-                value = calculate(operador=value[-1], valor_atual=valor_atual)
-
-            resultado.value = value
-            resultado.update()
+        
+        if value == 'AC':
+            resultado.value = '0'
+        
+        elif value.isdigit():
+            if valor_atual == '0' or valor_atual == 'Erro':
+                resultado.value = value
+            else:
+                resultado.value = valor_atual + value
+        
+        elif value in ['+', '-', '*', '/', '.']:
+            if valor_atual == 'Erro':
+                resultado.value = '0' + value
+            elif valor_atual[-1] in ['+', '-', '*', '/', '.']:
+                resultado.value = valor_atual[:-1] + value
+            else:
+                resultado.value = valor_atual + value
+        
+        elif value in ['=', '%', '±']:
+            if valor_atual != 'Erro':
+                resultado.value = calculate(operador=value, valor_atual=valor_atual)
+        
+        resultado.update()
 
     display = ft.Row(
         width=430,
@@ -77,19 +85,17 @@ def main (page: ft.Page):
         height=90,
         border_radius=100,
         bgcolor=btn['fundo'],
-        alignment= ft.alignment.center,
-        on_click = select
+        alignment=ft.alignment.center,
+        on_click=select
     ) for btn in botoes]
 
     keyboard = ft.Row(
         width=430,
-        wrap= True,
+        wrap=True,
         controls=botao,
-        alignment= 'end'
+        alignment='end'
     )
 
     page.add(display, keyboard)
 
-
-
-ft.app(target= main)
+ft.app(target=main)
